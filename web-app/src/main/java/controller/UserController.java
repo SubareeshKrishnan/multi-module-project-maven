a4f7aa760b9aa2247e1b68863566076af6803ef5
Subareeshkrishnan <subareeshkrishnan@gmail.com>
2022-05-08 10:49:54 +0530

    MMM - Web App Module

---
+++ web-app/src/main/java/controller/UserController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import com.orion.domain.UserCommand;
+import com.orion.entities.User;
+
+public class UserController {
+
+    User saveUser(UserCommand command){
+        return new User();
+    }
+
+}
